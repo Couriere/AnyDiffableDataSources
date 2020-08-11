2c50cddd57bb0e0488a96c549e6b1000677eb573
Vladimir Kazantsev <8406740@gmail.com>
2020-08-11 21:09:20 +0300

    Dependencies update.

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-	name: "AnyDiffabeDataSources",
+	name: "AnyDiffableDataSources",
 	platforms: [ .iOS( .v10 ), .tvOS( .v10 ) ],
 	products: [
-		.library( name: "AnyDiffabeDataSources", targets: ["AnyDiffabeDataSources"] ),
-		.library( name: "ReactiveAnyDiffabeDataSources", targets: ["ReactiveAnyDiffabeDataSources"] ),
+		.library( name: "AnyDiffableDataSources", targets: ["AnyDiffableDataSources"] ),
+		.library( name: "ReactiveAnyDiffableDataSources", targets: ["ReactiveAnyDiffableDataSources"] ),
 	],
 	dependencies: [
 		.package(url: "https://github.com/ra1028/DiffableDataSources", from: "0.4.0"),
-		.package(url: "https://github.com/ReactiveCocoa/ReactiveCocoa", from: "10.2.0"),
+		.package(url: "https://github.com/ReactiveCocoa/ReactiveCocoa", from: "11.0.0"),
 	],
 	targets: [
-		.target( name: "AnyDiffabeDataSources", dependencies: ["DiffableDataSources"], path: "Sources" ),
-		.target( name: "ReactiveAnyDiffabeDataSources", dependencies: ["AnyDiffabeDataSources", "ReactiveCocoa"], path: "ReactiveExtensions" ),
+		.target( name: "AnyDiffableDataSources", dependencies: ["DiffableDataSources"], path: "Sources" ),
+		.target( name: "ReactiveAnyDiffableDataSources", dependencies: ["AnyDiffableDataSources", "ReactiveCocoa"], path: "ReactiveExtensions" ),
 	],
 	swiftLanguageVersions: [ .v5 ]
 )
